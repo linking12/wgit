74420b88258586a0d3fffd1b6ab4fb5bacdf0a51
liushiming <liushiming@pajk.cn>
2014-07-29 16:36:06 +0800

    update

--- wgit/src/main/java/com/pajk/wgit/core/op/IWGitOperation.java
+++ wgit/src/main/java/com/pajk/wgit/core/op/IWGitOperation.java
@@ -71,20 +71,6 @@ public interface IWGitOperation {
 		public void setMessage(String message) {
 			this.message = message;
 		}
-
-		public String toJson() {
-			StringBuilder json = new StringBuilder();
-			if (resultCode.equals("000")) {
-				json.append("{\"code\":000");
-				json.append(",\"content\":\"").append(message).append("\"");
-				json.append("}");
-			} else {
-				json.append("{\"code\":").append(resultCode);
-				json.append(",\"error\":\"").append(message).append("\"");
-				json.append("}");
-			}
-			return json.toString();
-		}
 	}
 
 }
